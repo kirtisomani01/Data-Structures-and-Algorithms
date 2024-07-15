bc7f78232fa17823b3463cab6ffb46c1471b4135
kirtisomani01 <66521245+kirtisomani01@users.noreply.github.com>
2024-07-15 22:33:39 +0530

    Create Fibonacci Series.cpp

---
+++ Fibonacci Series.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cin>>num;
+    int a = 0, b = 1;
+    int nextterm;
+    for(int i = 2; i<num; i++){
+        nextterm = a+b;
+        a = b;
+        b = nextterm;
+        cout<<nextterm<<" ";
+    }
+    return 0;
+}
